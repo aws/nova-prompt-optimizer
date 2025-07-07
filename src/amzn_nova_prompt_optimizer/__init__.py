6c70c088178eafe4c173e57c89cca7836977cbdd
Vikram Shenoy <>
2025-07-07 11:07:11 -0400

    Initial Commit: Base launch of Nova Prompt Optimizer

---
+++ src/amzn_nova_prompt_optimizer/__init__.py
@@ -0,0 +1,4 @@
+"""NovaPromptOptimizer package."""
+from amzn_nova_prompt_optimizer.util.logging_utils import configure_nova_po_loggers
+
+configure_nova_po_loggers(__name__)
---
+++ src/amzn_nova_prompt_optimizer/__version__.py
@@ -0,0 +1,1 @@
+VERSION = "1.0.20250707160306"  # pragma: no cover
